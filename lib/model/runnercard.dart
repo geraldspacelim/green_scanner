1867eb39aa4adab51c64598149a952973d8d6f7f
Teng Fone <phangtf@gmail.com>
2020-06-19 17:27:02 +0800

    added the side way runner card thing

---
+++ lib/model/runnercard.dart
@@ -0,0 +1,26 @@
+class RunnerCard{
+    final String points, items, imagePath;
+
+    RunnerCard({
+        this.points,
+        this.items,
+        this.imagePath,
+    });
+}
+
+final runnercard = [
+RunnerCard(
+points: "50",
+items: "Milk",
+imagePath: "assets/milk.jpg",
+),
+RunnerCard(
+points: "200",
+items: "Shoes",
+imagePath: "assets/shoes.jpg",
+),
+RunnerCard(
+points: "100",
+items: "Takeaway",
+imagePath: "assets/food.jpeg",
+),];
--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,7 +1,10 @@
 import 'package:barcode_scan/barcode_scan.dart';
+import 'package:flip_card/flip_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'package:green_scanner/model/runnercard.dart';
 import 'package:rflutter_alert/rflutter_alert.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
 
 import '../../login.dart';
 import '../../login.dart';
@@ -15,10 +18,33 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
 
+  List<charts.Series<Task,String>> _seriesPieData;
+
+  _generateData(){
+    var pieData = [
+      new Task("Run", 20, Colors.green),
+      new Task("Jump", 80, Colors.black),
+    ];
+
+    _seriesPieData.add(
+      charts.Series(
+        data: pieData,
+        domainFn: (Task task,_) => task.task,
+        measureFn: (Task task,_) => task.taskvalue,
+        colorFn: (Task task, _) =>
+            charts.ColorUtil.fromDartColor(task.colorval),
+        id: 'Daily',
+        labelAccessorFn: (Task row,_)=>'${row.taskvalue}',
+      )
+    );
+  }
+
   @override
   void initState(){
     _updateImage();
     super.initState();
+    _seriesPieData = List<charts.Series<Task,String>>();
+    _generateData();
   }
 
   String qrResult = "Not Yet";
@@ -93,16 +119,112 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+
     return Scaffold(
       body: Stack(
         children: <Widget>[
-          new Container(
-            padding: const EdgeInsets.all(20.0),
-            child: new Column(
-              children: <Widget>[
-                new Text(qrResult),
-                new Image(image: _setImage ?? new AssetImage("assets/real_tree.gif")),
-              ],
+          Positioned(
+            top: 0,
+            height: height * 0.35,
+            left: 0,
+            right: 0,
+            child: ClipRRect(
+              borderRadius: const BorderRadius.vertical(
+                bottom: const Radius.circular(40),
+              ),
+              child: Container(
+                color: Colors.white,
+                child: new Column(
+                  children: <Widget>[
+                    new Image(image: _setImage ?? new AssetImage("assets/real_tree.gif"),),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            top: height * 0.38,
+            left: 0,
+            right: 0,
+            child: Container(
+              height: height * 0.5,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(
+                    bottom: 8,
+                      left: 32,
+                      right: 16,
+                    ),
+                    child: Text(
+                      "Recent Points",
+                      style: const TextStyle(
+                        color: Colors.blueGrey,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: <Widget>[
+                          SizedBox(
+                            width: 32,
+                          ),
+                          for (int i = 0; i<runnercard.length; i++)
+                            _RunnerCard(runnerCard: runnercard[i],)
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20,),
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.only(bottom: 10, left: 32, right: 32,),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(30)),
+                        gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [
+                            const Color(0xFF20008B),
+                            const Color(0XFF200087),
+                          ],
+                        )
+                      ),
+                        child: charts.PieChart(
+                          _seriesPieData,
+                          animate: true,
+                          animationDuration: Duration(milliseconds: 500),
+                          behaviors: [
+                            new charts.DatumLegend(
+                              outsideJustification: charts.OutsideJustification.startDrawArea,
+                              horizontalFirst: false,
+                              desiredMaxRows: 2,
+                              cellPadding: new EdgeInsets.only(right: 4.0, bottom: 4.0),
+                              entryTextStyle: charts.TextStyleSpec(
+                                  color: charts.MaterialPalette.purple.shadeDefault,
+                                  fontFamily: 'Georgia',
+                                  fontSize: 11),
+                            )
+                          ],
+                          defaultRenderer: new charts.ArcRendererConfig(
+                              arcWidth: 100,
+                              arcRendererDecorators: [
+                                new charts.ArcLabelDecorator(
+                                    labelPosition: charts.ArcLabelPosition.inside),
+                              ]
+                          ),
+                        ),
+
+                    )
+                  )
+                ],
+              ),
             ),
           ),
           Positioned(
@@ -198,4 +320,86 @@ class _HomeState extends State<Home> {
     ),
   );
 
+}
+
+class Task{
+  String task;
+  double taskvalue;
+  Color colorval;
+
+  Task(this.task,this.taskvalue,this.colorval);
+}
+
+class _RunnerCard extends StatelessWidget {
+
+  final RunnerCard runnerCard;
+
+  const _RunnerCard({Key key, this.runnerCard}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(right: 20,bottom: 10,),
+      child: Material(
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+        elevation: 4,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Flexible(
+              fit: FlexFit.tight,
+              child: ClipRRect(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                child: Image.asset(
+                  runnerCard.imagePath,
+                  width: 150,
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+            Flexible(
+              fit: FlexFit.tight,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text("Points",
+                    style: const TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 14,
+                      color: Colors.blueGrey
+                    ),),
+                    Text(runnerCard.points, style: const TextStyle(
+                      fontWeight: FontWeight.w700,
+                      fontSize: 18,
+                      color: Colors.black,
+                    )),
+                    Row(
+                      children: <Widget>[
+                        Icon(Icons.shopping_cart, color: Colors.black12,size: 20,),
+                        SizedBox(width: 4,),
+                        Text("${runnerCard.items}",
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 14,
+                          color: Colors.blueGrey,
+                        ),)
+                      ],
+                    ),
+                    SizedBox(height:5),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
